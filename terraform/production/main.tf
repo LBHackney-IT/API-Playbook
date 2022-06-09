4cb57ae8a31cf85a736b31b6ff3bf63bcae94ad5
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-06-09 16:59:39 +0100

    test pipeline

---
+++ terraform/production/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region  = "eu-west-2"
+  version = "~> 2.0"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-development-apis"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "common/state"
+  }
+}
+
+module "playbook_distribution" {
+  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/playbook-hosting?ref=playbook-distro"
+  cname_aliases = []
+  environment_name = "production"
+  cost_code = "B0811"
+  project_name = "hackney-playbooks"
+  use_cloudfront_cert = true
+}
