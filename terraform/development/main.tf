117da7cf599a508952f1d3b4544dfd2adb38ae47
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-06-14 16:07:36 +0100

    re-add module

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -12,11 +12,11 @@ provider "aws" {
   version = "~> 2.0"
 }
 
-# module "playbook_distribution" {
-#   source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/playbook-hosting?ref=playbook-distro"
-#   cname_aliases       = []
-#   environment_name    = "development"
-#   cost_code           = "B0811"
-#   project_name        = "hackney-playbooks"
-#   use_cloudfront_cert = true
-# }
+module "playbook_distribution" {
+  source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/playbook-hosting?ref=playbook-distro"
+  cname_aliases       = []
+  environment_name    = "development"
+  cost_code           = "B0811"
+  project_name        = "hackney-playbooks"
+  use_cloudfront_cert = true
+}
