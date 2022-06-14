782fa82fc3f640ad955fe848170ef5bc0e4ee3a7
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-06-14 16:05:36 +0100

    update backend

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -1,21 +1,15 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.0"
-    }
-  }
-
   backend "s3" {
     bucket  = "terraform-state-development-apis"
     encrypt = true
     region  = "eu-west-2"
-    key     = "common/state"
+    key     = "services/hackney-playbook/state"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+  version = "~> 2.0"
 }
 
 # module "playbook_distribution" {
