bc9960589f89c5dfa6ed671fc0574d133e8b3674
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2022-06-23 17:01:38 +0100

    working text to speech implementation

--- src/SpeechComponent.js
+++ src/SpeechComponent.js
@@ -1,32 +1,22 @@
 import React from "react"
 
 export default function TextToSpeech() {
+    
+    if ( 'speechSynthesis' in window )
+        {
+        const textInput = document.querySelector('.markdown').textContent
+        var to_speak = new SpeechSynthesisUtterance(textInput);
+        // window.speechSynthesis.speak(to_speak);
+        }
+
+        
+    
     return ( 
-      <div>
-    <form>
-                    <div className="form-group">
-                        <textarea name="" id="text-input" className="form-control form-control-lg" placeholder="Type to see the magic...."></textarea>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="rate">Rate</label>
-                        <div id="rate-value" className="badge badge-primary float-right">1</div>
-                        <input type="range" id="rate" className="custom-range" min="0.5" max="2" defaultValue="1" step="0.1">
-                            </input>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="pitch">Pitch</label>
-                        <div id="pitch-value" className="badge badge-primary float-right">1</div>
-                        <input type="range" id="pitch" className="custom-range" min="0" max="2" defaultValue="1" step="0.1">
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <select id="voice-select" className="form-control form-control-lg"></select>
-                    </div>
-                    <button className="btn btn-lite btn-lg btn-block">Speak It Out Loud</button>
-                    {console.log(synth)}
-                </form>
-                  <script src="src/textToSpeech.js"></script>
-                  </div>
+
+        <div>
+        <button>Play</button>
+        <button>Pause</button>
+    </div>
                   
     )
   }
--- src/textToSpeech.js
+++ src/textToSpeech.js
@@ -1,102 +1,51 @@
-// Init SpeechSynth API
-const synth = window.speechSynthesis;
+var $buttons = document.createElement('p'),
+      $button = document.createElement('button'),
+      $play = $button.cloneNode(),
+      $pause = $button.cloneNode(),
+      paused = false,
+      to_speak;
+	
+	if ( 'speechSynthesis' in window ) {
 
-
-// DOM Elements
-const textForm = document.querySelector("form");
-const textInput = document.querySelector("#text-input");
-const voiceSelect = document.querySelector("#voice-select");
-const rate = document.querySelector("#rate");
-const rateValue = document.querySelector("#rate-value");
-const pitch = document.querySelector("#pitch");
-const pitchValue = document.querySelector("#pitch-value");
-const body = document.querySelector("body");
-
-// Init voices array
-let voices = [];
-
-const getVoices = () => {
-  voices = synth.getVoices();
-  console.log('voices');
-
-  // Loop through voices and create an option for each one
-  voices.forEach(voice => {
-    // Create option element
-    const option = document.createElement("option");
-    // Fill option with voice and language
-    option.textContent = voice.name + "(" + voice.lang + ")";
-
-    // Set needed option attributes
-    option.setAttribute("data-lang", voice.lang);
-    option.setAttribute("data-name", voice.name);
-    voiceSelect.appendChild(option);
-  });
-};
-
-getVoices();
-if (synth.onvoiceschanged !== undefined) {
-  synth.onvoiceschanged = getVoices;
-}
-
-//Speech function
-const speak = () => {
-  //Check if allready speaking
-  if (synth.speaking) {
-    console.error("Already speaking...");
-    return;
-  }
-
-  //Check if the textarea is not empty
-  if (textInput.value !== "") {
-    //Add background image when speaking
-    body.style.background = "#141414 url(img/wave.gif)";
-    body.style.backgroundRepeat = "repeat-x";
-    body.style.backgroundSize = "100% 100%";
-
-    //Get the text to speak
-    const speakText = new SpeechSynthesisUtterance(textInput.value);
-    //Speak end
-    speakText.onend = e => {
-      console.log("Done speaking");
-      body.style.background = "#141414";
-    };
-    //Speak error
-    speakText.onerror = e => {
-      console.log("Something went wrong");
-    };
-    //Selected voice
-    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
-      "data-name"
+    window.addEventListener('beforeunload',function(){ window.speechSynthesis.cancel(); });
+    
+	  // content to speak
+    to_speak = new SpeechSynthesisUtterance(
+      document.querySelector('.markdown').textContent
     );
-    //Loop through voice
-    voices.forEach(voice => {
-      if (voice.name === selectedVoice) {
-        speakText.voice = voice;
+    
+    // set the rate a little faster than 1x
+    to_speak.rate = 1;
+	
+    // Event Handlers
+    function play() {
+      if ( paused ) {
+        paused = false;
+        window.speechSynthesis.resume();
+      } else {
+        window.speechSynthesis.speak( to_speak );
       }
-    });
+    }
+    function pause() {
+      paused = true;
+      window.speechSynthesis.pause();
+    }
+    
+    // Play Button
+    $play.innerText = 'Play';
+    $play.addEventListener( 'click', play, false );
+    $buttons.appendChild( $play );
+    
+    // Pause Button
+    $pause.innerText = 'Pause';
+    $pause.addEventListener( 'click', pause, false );
+	  $buttons.appendChild( $pause );
 
-    //set pitch and rate
-    speakText.rate = rate.value;
-    speakText.pitch = pitch.value;
+	} else {
+	  
+	  // Sad panda
+	  $buttons.innerText = 'Unfortunately your browser doesn’t support this feature.';
+	  
+	}
 
-    //Speak
-    synth.speak(speakText);
-  }
-};
-
-//Event Listener
-//submitting the text form
-textForm.addEventListener("submit", e => {
-  e.preventDefault();
-  speak();
-  textInput.blur();
-});
-
-//changing the rate value change
-rate.addEventListener("change", e => (rateValue.textContent = rate.value));
-
-//changing the pitch value change
-pitch.addEventListener("change", e => (pitchValue.textContent = pitch.value));
-
-//voice select change
-voiceSelect.addEventListener("change", e => speak());
+	document.body.appendChild( $buttons );
