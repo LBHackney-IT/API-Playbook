d18be21872ba1d77267d94d20e62b8982d1d829c
elena-evans <elena.evans@hackney.gov.uk>
2022-06-28 10:10:09 +0100

    completed text to speech functionality, with ability to read whole page or sections
    Will investigate being able to pause (not supported using current package)

--- src/SpeechComponent.js
+++ src/SpeechComponent.js
@@ -1,23 +1,43 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
-export default function TextToSpeech() {
-    
-    if ( 'speechSynthesis' in window )
-        {
-        const textInput = document.querySelector('.markdown').textContent
-        var to_speak = new SpeechSynthesisUtterance(textInput);
-        // window.speechSynthesis.speak(to_speak);
+export default function TextToSpeech({ children }) {
+    const [textToRead, setTextToRead] = useState('');
+    const [isHovering, setHovering] = useState(false);
+    const { speak, cancel, speaking, supported } = useSpeechSynthesis();
+
+    useEffect(() => {
+        const getNodeText = node => {
+            if (['string', 'number'].includes(typeof node)) return node
+            if (node instanceof Array) return node.map(getNodeText).join('')
+            if (typeof node === 'object' && node) return getNodeText(node.props.children)
         }
 
-        
-    
-    return ( 
+        setTextToRead(getNodeText(children));
+    }, []);
 
-        <div>
-        <button>Play</button>
-        <button>Pause</button>
-    </div>
-                  
-    )
-  }
-
+    if (supported && textToRead) {
+        return (
+            <div class={(isHovering || speaking) && "focussed-section"}>
+                <button 
+                    class="text-to-speech-buttons"
+                    onClick={() => speak({ text: textToRead })} 
+                    onMouseOver={() => setHovering(true)}
+                    onMouseOut={() => setHovering(false)}
+                >
+                    Listen to this section
+                </button>
+                <button 
+                    class="text-to-speech-buttons"
+                    onClick={cancel}
+                >
+                    Cancel
+                </button>
+                {children}
+            </div>
+        );
+    }
+    else {
+        return (<> </>);
+    }
+}
