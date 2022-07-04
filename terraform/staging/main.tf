50bdb3bfc947c9054b4ba286add9a501b87f5822
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-07-04 14:25:18 +0100

    Add cname aliases

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -14,9 +14,10 @@ provider "aws" {
 
 module "playbook_distribution" {
   source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/playbook-hosting/without-waf?ref=playbook-distro"
-  cname_aliases       = []
+  cname_aliases       = ["playbook-staging.hackney.gov.uk"]
   environment_name    = "staging"
   cost_code           = "B0811"
   project_name        = "hackney-playbooks"
-  use_cloudfront_cert = true
+  use_cloudfront_cert = false
+  hackney_cert_arn    = "arn:aws:acm:us-east-1:${data.aws_caller_identity.current.account_id}:certificate/8f7fa30c-a4e5-4775-b827-ade824a33c9a"
 }
