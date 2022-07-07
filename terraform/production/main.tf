4571d56604800563da98f63d499b456b5cfa6d63
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-07-07 10:29:53 +0100

    Add origin var

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -14,13 +14,21 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+locals {
+  origins = {
+    "tech-radar-frontend-${var.environment_name}"   = "tech-radar"
+    "api-playbook-frontend-${var.environment_name}" = "API-Playbook"
+  }
+}
+
 module "playbook_distribution" {
-  source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/playbook-hosting/with-waf?ref=playbook-distro"
+  source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/playbook-hosting/with-waf?ref=update-playbook"
   cname_aliases       = ["playbook.hackney.gov.uk"]
   environment_name    = "production"
   cost_code           = "B0811"
   project_name        = "hackney-playbooks"
   use_cloudfront_cert = false
+  origins             = local.origins
   hackney_cert_arn    = "arn:aws:acm:us-east-1:${data.aws_caller_identity.current.account_id}:certificate/4877809f-989f-4e31-9a1a-4c054f00b51e"
   web_acl_id = "arn:aws:wafv2:us-east-1:${data.aws_caller_identity.current.account_id}:global/webacl/FE_Web_ACL/679f5ebc-fb36-48ae-88c3-be4b55598720"
 }
--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -14,12 +14,20 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+locals {
+  origins = {
+    "tech-radar-frontend-${var.environment_name}"   = "tech-radar"
+    "api-playbook-frontend-${var.environment_name}" = "API-Playbook"
+  }
+}
+
 module "playbook_distribution" {
-  source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/playbook-hosting/without-waf?ref=playbook-distro"
+  source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/playbook-hosting/without-waf?ref=update-playbook"
   cname_aliases       = [] // "playbook-staging.hackney.gov.uk"
   environment_name    = "staging"
   cost_code           = "B0811"
   project_name        = "hackney-playbooks"
   use_cloudfront_cert = true
+  origins             = local.origins
   hackney_cert_arn    = "arn:aws:acm:us-east-1:${data.aws_caller_identity.current.account_id}:certificate/" // Update once certificate is made
 }
