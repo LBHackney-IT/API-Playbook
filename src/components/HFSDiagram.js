111660609ae2ae6182985aad7c31a5649f5837ae
Faisal Gazi <faisal.gazi@hackney.gov.uk>
2024-02-01 20:14:34 +0000

    Changed HFS component to a hierarchy diagram

--- src/components/HFSDiagram.js
+++ src/components/HFSDiagram.js
@@ -1,9 +1,9 @@
 import React from "react";
-import D3Tree from "./D3Tree";
+import D3Hierarchy from "./D3Hierarchy";
 import hfsData from "../../assets/hfs_db.json";
 
 const HFSDiagram = () => {
-    return <D3Tree data={hfsData} />;
+    return <D3Hierarchy data={hfsData} />;
 };
 
 export default HFSDiagram;
