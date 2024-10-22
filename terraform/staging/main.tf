f20128844080d6d590ceaec7182d871173fd9dd9
Ryan Brooks <ryan@slatehorse.com>
2024-10-22 11:51:23 +0100

    Destroy legacy infrastructure
    These resources aren't needed anymore as we're using GitHub Pages to host playbooks directly.

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -21,14 +21,3 @@ locals {
     "api-playbook-frontend-${local.environment_name}" = "API-Playbook"
   }
 }
-
-module "playbook_distribution" {
-  source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/playbook-hosting/without-waf?ref=playbook-distro"
-  cname_aliases       = [] // "playbook-staging.hackney.gov.uk"
-  environment_name    = local.environment_name
-  cost_code           = "B0811"
-  project_name        = "hackney-playbooks"
-  use_cloudfront_cert = true
-  origins             = local.origins
-  hackney_cert_arn    = "arn:aws:acm:us-east-1:${data.aws_caller_identity.current.account_id}:certificate/" // Update once certificate is made
-}
